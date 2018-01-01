832f7732613e07e543d7e6ab98633012cb2acb17
Ross Kaffenberger <rosskaff@gmail.com>
2018-01-01 03:08:21 -0500

    Refactor Winner algorithm

--- apps/connect_four/lib/connect_four/winner.ex
+++ apps/connect_four/lib/connect_four/winner.ex
@@ -1,54 +1,64 @@
 defmodule ConnectFour.Winner do
-  alias ConnectFour.Board
+  alias ConnectFour.{Board, Winner}
+
+  @down {-1, 0}
+  @right {0, +1}
+  @down_right {-1, +1}
+  @rise_right {+1, +1}
+
+  defstruct [
+    moves: [],
+    delta: nil,
+    color: :empty,
+  ]
 
   require Logger
 
   def winner(%Board{last: nil}), do: nil
-  def winner(%Board{last: last} = board) do
-    column_winner(board, last) || row_winner(board, last) || diagonal_winner(board, last)
+  def winner(%Board{last: last} = board), do: winner(board, last)
+  def winner(_board, {_row, _col, :empty}), do: nil
+  def winner(%Board{} = board, checker) do
+    column_winner(board, checker) || row_winner(board, checker) || diagonal_winner(board, checker)
   end
 
   defp column_winner(_board, {row, _col, _color}) when row + 1 < 4, do: nil
-  defp column_winner(_board, {_row, _col, :empty}), do: nil
-  defp column_winner(board, {row, col, color} = checker) do
-    column_winner(board, checker, color, [{row, col}])
+  defp column_winner(board, {_row, _col, _color} = checker) do
+    collect_winner(board, checker, @down)
   end
-  defp column_winner(_board, _checker, color, moves) when length(moves) == 4, do: {color, moves}
-  defp column_winner(board, {row, col, color}, color, moves) do
-    column_winner(board, Board.checker(board, {row-1, col}), color, [{row-1, col} | moves])
-  end
-  defp column_winner(_board, _checker, _color, _moves), do: nil
 
-  defp row_winner(_board, {_row, _col, :empty}), do: nil
-  defp row_winner(board, {_row, _col, color} = checker) do
-    row_winner(board, winner_start_checker(board, checker), color, [])
+  defp row_winner(board, {_row, _col, _color} = checker) do
+    collect_winner(board, checker, @right)
   end
-  defp row_winner(_board, _checker, color, moves) when length(moves) == 4, do: {color, moves}
-  defp row_winner(board, {row, col, color}, color, moves) do
-    row_winner(board, Board.checker(board, {row, col+1}), color, [{row, col} | moves])
-  end
-  defp row_winner(_board, _checker, _color, _moves), do: nil
 
-  defp diagonal_winner(_board, {_row, _col, :empty}), do: nil
-  defp diagonal_winner(board, {_row, _col, color} = checker) do
-    diagonal_winner(board, winner_start_checker(board, checker, -1), +1, color, [])
-    || diagonal_winner(board, winner_start_checker(board, checker, +1), -1, color, [])
+  defp diagonal_winner(board, {_row, _col, _color} = checker) do
+    collect_winner(board, checker, @rise_right) || collect_winner(board, checker, @down_right)
   end
-  defp diagonal_winner(_board, _checker, _row_diff, color, moves) when length(moves) == 4, do: {color, moves}
-  defp diagonal_winner(board, {row, col, color}, row_diff, color, moves) do
-    diagonal_winner(board, Board.checker(board, {row+row_diff, col+1}), row_diff, color, [{row, col} | moves])
-  end
-  defp diagonal_winner(_board, _checker, _diff, _color, _moves), do: nil
 
-  defp winner_start_checker(board, checker), do: winner_start_checker(board, checker, 0)
-  defp winner_start_checker(board, {row, col, color}, row_diff) do
-    row_left = row+row_diff
-    col_left = col-1
+  defp collect_winner(board, {_row, _col, color} = checker, delta) do
+    first_checker = winner_start_checker(board, checker, inverse_delta(delta))
+
+    %Winner{color: color, delta: delta}
+    |> collect_winner(board, first_checker)
+  end
+  defp collect_winner(%Winner{moves: moves, color: color}, _board, _checker) when length(moves) == 4, do: {color, moves}
+  defp collect_winner(%Winner{color: color} = winner, board, {row, col, color}) do
+    next_checker = Board.checker(board, delta_move({row, col}, winner.delta))
+
+    %Winner{winner | moves: [{row, col} | winner.moves]}
+    |> collect_winner(board, next_checker)
+  end
+  defp collect_winner(_winner, _board, _checker), do: nil
+
+  defp winner_start_checker(board, {row, col, color}, delta) do
+    {row_left, col_left} = delta_move({row, col}, delta)
     case Board.checker(board, {row_left, col_left}) do
       {row_left, col_left, ^color} ->
-        winner_start_checker(board, {row_left, col_left, color}, row_diff)
+        winner_start_checker(board, {row_left, col_left, color}, delta)
       _ ->
         {row, col, color}
     end
   end
+
+  defp inverse_delta({row_diff, col_diff}), do: {row_diff * -1, col_diff * -1}
+  defp delta_move({row, col}, {row_diff, col_diff}), do: {row+row_diff, col+col_diff}
 end
